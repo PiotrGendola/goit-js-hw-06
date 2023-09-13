de410e31b80752155bb7aee1a92d3db34e584984
Piotr Gendola <piotrgendola@gmail.com>
2023-09-13 22:36:28 +0200

    e

--- js/task-06.js
+++ js/task-06.js
@@ -1,7 +1,7 @@
 const inputText = document.querySelector("input#validation-input");
 const inputValidColor = document.querySelector("input#validation-input.valid");
 const validLength = parseInt(inputText.getAttribute("data-length"));
-console.log(inputText);
+
 inputText.addEventListener("blur", (event) => {
   if (event.currentTarget.value.length === validLength) {
     console.log("OK");
